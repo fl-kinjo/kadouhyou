830acdb44886a5393a97af7bec2d1b6a6964e7f0
fl-kinjo <kinjo@framelunch.jp>
2026-03-25 19:13:52 +0900

    ログイン処理修正

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,21 @@
+// app/auth/callback/route.ts
+
+import { NextResponse } from "next/server";
+import { createClient } from "@/app/utils/supabase/server";
+
+export async function GET(request: Request) {
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get("code");
+
+  if (code) {
+    const supabase = await createClient();
+
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+    if (!error) {
+      return NextResponse.redirect(`${origin}/summary`);
+    }
+  }
+
+  return NextResponse.redirect(`${origin}/login`);
+}
